b3fef6ab0a49829e51641eb771b7af53ba91d89e
allenwhale <allenwhale@yahoo.com.tw>
2015-04-09 02:28:35 +0800

    add index

--- backend/server.py
+++ backend/server.py
@@ -7,14 +7,23 @@ from req import Service
 from req import reqenv
 from req import RequestHandler
 import config
+import json
 
 import pg
+
+class IndexHandler(RequestHandler):
+    @reqenv
+    def get(self):
+        self.finish(json.dumps({'version': 0.1}))
+        return
+
 
 if __name__ == '__main__':
     httpsock = tornado.netutil.bind_sockets(config.PORT)
     tornado.process.fork_processes(config.PROCESSES)
     db = pg.AsyncPG(config.DBNAME,config.DBUSER,config.DBPASSWORD,dbtz='+8')
     app = tornado.web.Application([
+        ('/apis/', IndexHandler),
         ],cookie_secret = 'cookie',autoescape = 'xhtml_escape')
     srv = tornado.httpserver.HTTPServer(app)
     srv.add_sockets(httpsock)
